79ef189374e0623613b1acae4c2b53a2ef2b7738
kheireddine <kheireddine.terfa@gmail.com>
2024-10-28 10:28:35 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Hello, Tailwind CSS with Vite + React!
+      </h1>
+    </div>
+  )
+}
+
+export default App
